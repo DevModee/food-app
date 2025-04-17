45195911dfde6c017fcc850f02b2152fd640dabb
Matias <caelien2.0@gmail.com>
2025-04-16 22:56:56 -0300

    refactor app to use authnavigation and add home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AuthNavitator from './navigation/AuthNavigation';
+import { createStackNavigator } from '@react-navigation/stack';
+import AuthNavigator from './navigation/AuthNavigation';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthNavitator />
+      <Stack.Navigator id={undefined} initialRouteName="Auth">
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+        <Stack.Screen name="Welcome" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
