8d066e6a8ad06f9596f7b5b2ed70ead2163a0d40
Matias <caelien2.0@gmail.com>
2025-04-16 23:20:17 -0300

    fix: endpoints change

--- api/api.ts
+++ api/api.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const api = 'https://ts-food-weight-api.onrender.com/api';
 
-export const login = async (email: string, password: string) => {
-  const res = await axios.post(`${api}/auth/login`, { email, password });
+export const login = async (username: string, password: string) => {
+  const res = await axios.post(`${api}/users/login`, { username, password });
   return res.data;
 }
 
-export const register = async (email: string, password: string) => {
-  const res = await axios.post(`${api}/auth/register`, { email, password });
+export const register = async (username: string, password: string) => {
+  const res = await axios.post(`${api}/users/register`, { username, password });
   return res.data;
 }
 
