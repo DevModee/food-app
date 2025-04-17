12d11f2894324d2a708de39c6c7b15d7aada4f90
Matias <caelien2.0@gmail.com>
2025-04-17 10:28:32 -0300

    Add WeightContext, HomeScreen with weight input and chart, and integrate global state for weight tracking

--- app/App.tsx
+++ app/App.tsx
@@ -3,16 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthNavigator from './navigation/AuthNavigator';
 import HomeScreen from './screens/HomeScreen';
+import { WeightProvider } from './context/WeightContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator id={undefined} initialRouteName="Auth" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Auth" component={AuthNavigator} />
-        <Stack.Screen name="Welcome" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <WeightProvider>
+      <NavigationContainer>
+        <Stack.Navigator id={undefined} initialRouteName="Auth" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Auth" component={AuthNavigator} />
+          <Stack.Screen name="Welcome" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </WeightProvider>
   );
 }
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,14 +1,119 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useWeightContext } from '../context/WeightContext';
+import { LineChart } from 'react-native-gifted-charts';
 
-const HomeScreen = ({ route }: any) => {
-  const { username } = route.params;
+const HomeScreen = () => {
+
+  const { username, weightData, addWeight } = useWeightContext();
+  const [weight, setWeight] = useState('')
+
+  const handleAddWeight = () => {
+    if (!weight) return;
+
+    const newEntry = {
+      value: parseFloat(weight),
+      date: new Date().toISOString(),
+    };
+
+    addWeight(newEntry);
+    setWeight('');
+  };
+
+  const chartData = weightData.map((entry) => ({
+    value: entry.value,
+    label: new Date(entry.date).getDate().toString(),
+  }));
 
   return (
-    <View>
-      <Text>Bienvenido, {username}!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido, {username}!</Text>
+      <Text style={styles.weightTitle}>
+        ¿Cuánto pesás hoy?
+      </Text>
+
+      <TextInput
+        placeholder="Ej: 70,.3"
+        keyboardType="numeric"
+        value={weight}
+        onChangeText={setWeight}
+        style={styles.weightForm}
+      />
+      <View style={styles.confirmationButton}>
+        <Button title="Agregar peso" onPress={handleAddWeight} />
+      </View>
+
+      {chartData.length > 0 && (
+        <View style={styles.weightChart}>
+          <Text style={styles.weightEvolution}>Evolución del peso</Text>
+          <LineChart
+            data={chartData}
+            curved
+            thickness={3}
+            color="#4a90e2"
+            hideDataPoints={false}
+            yAxisTextStyle={{ color: '#4a90e2' }}
+          />
+        </View>
+      )}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: `#a3b8f0`,
+    padding: 20,
+    paddingTop: 50,
+  },
+  title: {
+    color: '#333',
+    fontSize: 30,
+    fontWeight: '700',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  weightTitle: {
+    color: '#333',
+    fontSize: 20,
+    fontWeight: '500',
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  weightForm: {
+    backgroundColor: '#ffffff',
+    color: '#333',
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+
+  },
+  confirmationButton: {
+    marginBottom: 20,
+  },
+  weightChart: {
+    backgroundColor: '#ffffff',
+    borderRadius: 16,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  weightEvolution: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 15,
+    color: '#333',
+  },
+
+});
 
 export default HomeScreen;
